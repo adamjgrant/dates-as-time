249f48e0f916b2761e9e5c2149e05ff1753d0e33
Adam Grant <hello@adamgrant.me>
2023-02-19 11:40:18 -0800

    Establishing what I'm guessing might be the right functions

---
+++ script.js
@@ -0,0 +1,6 @@
+const get_current_local_date_time = () => {}
+const get_date_time_in_seconds_past_midnight = () => {}
+const get_monthiness_for_seconds_past_midnight = () => {}
+const get_dayiness_for_seconds_past_midnight = () => {}
+const get_houriness_for_seconds_past_midnight = () => {}
+const get_minutiness_for_seconds_past_midnight = () => {}
